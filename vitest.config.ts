ae1bc9b7eb5872ee8ad91a34e5084d32ac21a9b7
Christian Cobian <109490056+jcris21@users.noreply.github.com>
2026-06-05 17:33:08 -0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/test/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-  },
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-});
+}));
